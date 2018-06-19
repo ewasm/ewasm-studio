bf547d73652cf5253a5349a881085b1a0652efa2
Jared Wasinger <j-wasinger@hotmail.com>
2018-06-19 23:05:17 +0000

    add material-ui for prettier interface

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Web3} from 'web3'
-import {binaryen} from 'binaryen'
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Icon from '@material-ui/core/Icon'
+
 import './App.css';
 
 class App extends Component {
@@ -10,12 +14,18 @@ class App extends Component {
  
     this.state = {
       wast: '',
-      placeholderText: "Enter transaction data"
+      anchorEl: null,
+      placeholderText: "Enter transaction data",
+      TxType: 'Transaction'
     }
 
     //alert(binaryen)
     this.handleChange = this.handleChange.bind(this)
-    this.onSelectChange = this.onSelectChange.bind(this)
+    //this.handleClose = this.handleClose.bind(this)
+    //this.onSelectChange = this.onSelectChange.bind(this)
+    this.setContract = this.setContract.bind(this)
+    this.setTx = this.setTx.bind(this)
+    this.onTx = this.onTx.bind(this)
 
     //this.handleChange = this.handleChange.bind(this);
     //this.handleSubmit = this.handleSubmit.bind(this);
@@ -55,7 +65,7 @@ class App extends Component {
         //TODO do something here
       }
 
-      if (this.state.placeholderText == 'enter contract code') {
+      if (this.state.TxType == 'Contract') {
         //contract creation transaction
 
         for (let i = 0; i < wasm.length; i += 2) {
@@ -63,7 +73,7 @@ class App extends Component {
         }
 
         console.log(wast)
-        wast = `(module (import "ethereum" "return" (func $return (param i32 i32))) (memory 100) (data (i32.const 0)  "${wast}") (export "memory" (memory 0)) (export "main" (func $main)) (func $main (call $return (i32.const 0) (i32.const ${wasm.length / 2}))))`
+        wast = `(module (import "ethereum" "return" (func $return (param i32 i32))) (memory 100) (data (i32.const 0)  "${wast}") (export "memory" (memory 0)) (export "main" (func $main)) (func $main (call $return (i32.const 0) (i32.const ${wasm.ength / 2}))))`
 
         try {
           let module = window.Binaryen.parseText(wast)
@@ -76,9 +86,34 @@ class App extends Component {
 
       this.state.web3.eth.sendTransaction({'from': '', 'data': wasm}, (e, tx) => {
         if (e) throw(e)
-        console.log("tx receiopt: "+tx)
+        /*
+        this.state.web3.eth.getTransactionReceipt(tx, (e, txn) => {
+          if (e) throw(e)
+          if (txn) {
+            cb(txn)
+
+          }
+          }
+        })
+        */
+        let state = this.state
+        let onTx = this.onTx
+
+        this.state.web3.eth.filter("latest", function(e, result) {
+          state.web3.eth.getTransactionReceipt(tx, (e, txn) => {
+            if (e) throw(e)
+            if (txn) {
+              onTx(tx)
+            }
+          })
+        })
+        //console.log("tx receiopt: "+tx)
       })
     })
+  }
+
+  onTx(tx) {
+    alert(tx.status === "1" ? "transaction succeeded" : "transaction failed")
   }
 
   componentWillMount() {
@@ -87,6 +122,7 @@ class App extends Component {
         web3: window.web3
       })
     })
+    this.setState({ anchorEl: null });
   }
 
   onSelectChange(e) {
@@ -95,7 +131,32 @@ class App extends Component {
     })
   }
 
+  handleClose = event => {
+    this.setState({ anchorEl: null});
+  };
+
+  setContract = event => {
+    this.setState({
+      placeholderText: "Enter contract code",
+      TxType: 'Contract'
+    })
+    this.setState({ anchorEl: null });
+  }
+
+  setTx = event => {
+    this.setState({
+      placeholderText: "Enter transaction data",
+      TxType: 'Transaction'
+    })
+    this.setState({ anchorEl: null });
+  }
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
   render() {
+    const {anchorEl} = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -105,16 +166,31 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <select name="Transaction Type" onChange={this.onSelectChange}>
-          <option value="enter transaction data">Transaction data</option>
-          <option value="enter contract code">Contract code</option>
-        </select>
+				<div>
+					<Button
+						aria-owns={anchorEl ? 'simple-menu' : null}
+						aria-haspopup="true"
+						onClick={this.handleClick}
+					>
+						{this.state.TxType}
+						<Icon right>expand_more</Icon>
+					</Button>
+					<Menu
+						id="simple-menu"
+						anchorEl={anchorEl}
+						open={Boolean(anchorEl)}
+						onClose={this.handleClose}
+					>
+						<MenuItem onClick={this.setTx}>Normal Transaction</MenuItem>
+						<MenuItem onClick={this.setContract}>Contract Creation</MenuItem>
+					</Menu>
+				</div>
         <div style={{width: "100%"}} >
           <textarea onChange={this.handleChange} style={{display: "block"}} rows="20" cols="80" placeholder={this.state.placeholderText} id="editor"></textarea>
         </div>
-        <button onClick={() => this.onSubmitTx()}>
+        <Button variant="contained" color="primary" onClick={() => this.onSubmitTx()}>
           Submit Tx
-        </button>
+        </Button>
       </div>
     );
   }
